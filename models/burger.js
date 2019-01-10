db3d87f39a19c2e48b69f0d1a52f3857c08fcb69
Scott <smcanally11@yahoo.com>
2019-01-09 20:44:54 -0500

    fix functions in burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,20 @@
 var express = require("express");
-var burger = require("../models/burger");
+var orm = require('../config/orm.js');
 
 var burger = {
-    all: function(cb) {
+    selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
 
-    create: function(cols, vals, cb) {
+    insertOne: function(cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
 
-    update: function(objColVals, condition, cb) {
+    updateOne: function(objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function(res) {
 
         });
